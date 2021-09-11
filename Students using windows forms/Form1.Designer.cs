7106caa0ee608a0a2a3da626b5176b9e497faf93
Alixander Lawan <alixander.lawan@gmail.com>
2021-09-12 00:51:59 +0800

    Changed form name

--- Students using windows forms/Form1.Designer.cs
+++ Students using windows forms/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace Students_using_windows_forms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Students";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
